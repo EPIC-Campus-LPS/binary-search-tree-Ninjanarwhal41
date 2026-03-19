cf3b31bf30f3e2abe907cd2be3cd37a853395726
root <elicohrs@gmail.com>
2026-03-19 15:06:09 -0600

    created template for BST class and main.

---
+++ src/BST.java
@@ -0,0 +1,30 @@
+public class BST {
+    public void add(E value){
+
+    }
+    public boolean contains(E value){
+        return false;
+    }
+    public int countNodes(){
+        return 0;
+    } //returns the number of nodes in the tree
+
+    int countLeafNodes(){
+        return 0;
+    } //returns the number of leaf nodes in tree
+    int getHeight(){
+        return 0;
+    } //returns the longest path from the root to a leaf node
+    void printInorder(){
+        System.out.println();
+    }
+    void printPreorder(){
+        System.out.println();
+    }
+    void printPostorder(){
+        System.out.println();
+    }
+    E delete(E value){
+        return null;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        
-
-        System.out.println("Hello world!");
+        /*
+        The function of this main program is to:
+        Fill the tree from a file
+        Add a value to tree
+        Delete a value from the tree
+        See if tree contains a value
+        Test traversals (pre, in, post)
+        Print stats (nodes, leaf nodes, height)
+        Clear the tree
+        Exit Program
+         */
     }
 }
