ca23b8b835a947953f0b8e0c2df6b7f07eb8ea17
root <elicohrs@gmail.com>
2026-03-30 15:06:03 -0600

    started working on methods.

--- src/BST.java
+++ src/BST.java
@@ -1,18 +1,67 @@
-public class BST {
+public class BST<E extends Comparable<E>> {
+    private TreeNode<E> root;
+    private TreeNode<E>[] data;
+    private int size;
+
+    public BST(TreeNode<E>[] data) {
+        this.data = data;
+        size = data.length;
+        root = data[0];
+    }
+
     public void add(E value){
+        TreeNode<E> temp = root;
+        TreeNode<E> newNode = new TreeNode<E>(value, null, null);
+        if(root == null){
+            root = newNode;
+            root.setLeftChild(null);
+            root.setRightChild(null);
+        }
+
+        if(value.compareTo(root.getValue()) > 0){
+            if (temp != null && temp.getLeftChild() == null){
+                temp.setLeftChild(newNode);
+            }
+            while(value.compareTo(temp.getValue()) > 0){
+
+            }
+        }
+        else{
+            if (temp != null && temp.getLeftChild() == null){
+                temp = temp.getLeftChild();
+            }
+
+        }
+
 
     }
     public boolean contains(E value){
         return false;
     }
     public int countNodes(){
-        return 0;
+        int count = 1;
+        TreeNode<E> temp = root;
+        if(root == null){
+            return 0;
+        }
+        if(root.getLeftChild() == null && root.getRightChild() == null){
+            return count;
+        }
+        while(temp != null && temp.getLeftChild() != null && temp.getRightChild() != null){
+            temp = temp.getLeftChild();
+            count++;
+        }
+        return count;
     } //returns the number of nodes in the tree
 
     int countLeafNodes(){
         return 0;
     } //returns the number of leaf nodes in tree
     int getHeight(){
+        if (root == null){
+            return 0;
+        }
+
         return 0;
     } //returns the longest path from the root to a leaf node
     void printInorder(){
--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,11 @@ public class Main {
         Clear the tree
         Exit Program
          */
+        TreeNode<Integer> a = new TreeNode<Integer>(3, null, null);
+        TreeNode<Integer> b = new TreeNode<Integer>(2, null, null);
+        TreeNode<Integer> c = new TreeNode<Integer>(1, null, null);
+
+        BST<Integer> bst = new BST<>(arr);
+
     }
 }
